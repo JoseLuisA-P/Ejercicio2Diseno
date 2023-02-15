ec0a61d66bed2ef1dacd27d1e487f5854f3f5c9c
Jose Alvarez <alv19392@uvg.edu.gt>
2023-02-15 08:43:34 -0600

    Agregando dos funcionalidades a la clase

--- RobotClass.py
+++ RobotClass.py
@@ -8,3 +8,13 @@ class Robot:
         self.color = color
         self.tamano = tamano
         self.comms = comms
+
+    def mover(self,distancia):
+        try:
+            print(f"El robot se movera a {distancia[0]}m en X y {distancia[1]}m en Y")
+            self.distanciaActual = distancia
+        except:
+            print("Revisa el valor ingresado de movimiento")
+
+    def hablar(self):
+        print(f"{self.ID} esta tratando de comunicarte algo por {self.comms}")
